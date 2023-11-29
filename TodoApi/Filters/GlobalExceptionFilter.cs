eb5376cfe651c6e5435b6e96f17c04ed09fe3b55
tpithani <tpithani@OSI-L-0399>
2023-11-29 12:28:13 +0530

    Add project files.

---
+++ TodoApi/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TodoApi.Models;
+
+namespace TodoApi.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var error = new ErrorDetails()
+            {
+                StatusCode = 500,
+                Message = "Something went wrong! Internal Server Error."
+            };
+            //Logs your technical exception with stack trace below
+            context.Result = new JsonResult(error);
+        }
+    }
+}
---
+++ TodoApi/Filters/LocalExceptionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TodoApi.Models;
+
+namespace TodoApi.Filters
+{
+    public class LocalExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var error = new ErrorDetails()
+            {
+                StatusCode = 501,
+                Message = "Something went wrong! Internal Server Error."
+            };
+            context.Result = new JsonResult(error);
+        }
+    }
+}
